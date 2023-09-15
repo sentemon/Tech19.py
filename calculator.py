c13b4ed0d81dd73328405786803fd2473a655d4d
Ivan <124244721+sentemon@users.noreply.github.com>
2023-09-15 20:52:30 +0200

    Update 2 calculator.py

--- calculator.py
+++ calculator.py
@@ -1,12 +1,33 @@
-a, b = int(input('Wpisz pierwszą liczbę')), int(input('Wpisz drugą liczbę')) #kalkulator
-x = input('Wybierz operacje: (+, -, *, /)')
-def calculator(a, b):
-  if x == '+':
-    print(f'{a} + {b} = {a + b}')
-  elif x == '-':
-    print(f'{a} - {b} = {a - b}')
-  elif x == '*':
-    print(f'{a} * {b} = {a * b}')
-  elif x == '/':
-    print(f'{a} / {b} = {a / b}')
-calculator(a, b)
+# Pobieranie dwóch liczb od użytkownika
+a = int(input('Wpisz pierwszą liczbę: '))
+b = int(input('Wpisz drugą liczbę: '))
+
+# Wybór operacji matematycznej
+x = input('Wybierz operację (+, -, *, /): ')
+
+# Funkcja kalkulatora
+def calculator(a, b, operator):
+    wynik = None  # Inicjalizacja zmiennej wynik
+
+    # Sprawdzanie wybranej operacji i wykonywanie odpowiedniego obliczenia
+    if operator == '+':
+        wynik = a + b
+    elif operator == '-':
+        wynik = a - b
+    elif operator == '*':
+        wynik = a * b
+    elif operator == '/':
+        # Obsługa dzielenia przez zero
+        if b == 0:
+            print('Nie można dzielić przez zero.')
+            return
+        wynik = a / b
+    else:
+        print('Nieprawidłowa operacja. Wybierz spośród: +, -, *, /')
+        return
+
+    # Wyświetlanie wyniku
+    print(f'{a} {operator} {b} = {wynik}')
+
+# Wywołanie funkcji kalkulatora
+calculator(a, b, x)
