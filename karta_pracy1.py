c636c59289f04f2750a82f451856d3faba742b9e
Ivan Sentemon <124244721+sentemon@users.noreply.github.com>
2023-09-20 16:03:57 +0200

    Update karta_pracy1.py

--- karta_pracy1.py
+++ karta_pracy1.py
@@ -9,7 +9,7 @@
 #print(f"({a} - {b})^3 = {(a - b)**3}")
             #zadanie 4
 #a, b, c = int(input()), int(input()), int(input())
-#print(f"{a} * {b} * {c} = {a*b*c}")
+#print(f"{a} * {b} * {c} = {a * b * c}")
             #zadanie 5
 #a, b = int(input()), int(input())
 #print(f"({a} + {b}) * 0.4 = {(a + b) * 0.4}")
@@ -18,4 +18,4 @@
 #print(f"{brutto} / 1.23 = {brutto / 1.23}")
             #zadanie 7
 #a, b = int(input()), int(input())
-#print(f"{a} % {b} = {a%b}")
+#print(f"{a} % {b} = {a % b}")
