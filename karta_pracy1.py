166465aaaf47306d5cd9498982ce2d7a2de173dc
Ivan Sentemon <124244721+sentemon@users.noreply.github.com>
2023-09-20 12:08:29 +0200

    Create karta_pracy1.py

---
+++ karta_pracy1.py
@@ -0,0 +1,9 @@
+            #zadanie 1
+#a, b = int(input()), int(input())
+#print(f"{a**2} + {b**2}" = {a**2 + b**2}") 
+            #zadanie 2
+#a, b = int(input()), int(input())
+#print(f"({a} + {b})^2 = {(a + b)**2}")
+            #zadanie 3
+#a, b = int(input()), int(input())
+#print(f"({a} - {b})^3 = {(a - b)**3}")
